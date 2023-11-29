b312a56f52d65de61eb68198556c68c121f0a7c4
Readytowork co. ltd <readytowork@Readytoworks-MacBook-Air.local>
2023-11-29 20:14:20 +0545

    first commit

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@nx-shadcnui/shared';
 export default async function Index() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-slate-600">
-      <h1 className="text-6xl text-white">Hello from web app</h1>
+      <h1 className="text-6xl text-white">Hello from docs app</h1>
       <Button variant={'destructive'}>Shared button</Button>
     </div>
   );
